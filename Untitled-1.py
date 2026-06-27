e9fb3712640c71c8763afd62a538116afb572c0f
CataTordecillaR <cata.tordecilla@gmail.com>
2026-06-26 20:11:56 -0400

    aa

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,18 @@
-print("Holaaaaaaaa")
+items = {
+    "MAG001" : ["Agonía de Escarcha", "Arma", "Legendaria", "Hielo"],
+    "MAG002" : ["Anillo de Sauron", "Accesorio", "Epica", "Magia"],
+    "MAG003" : ["Poción curativa", "Consumible", "Comun", "Vida"],
+    
+}
+
+while True:
+
+    print("=== ALDRICEL COMERCIANTE ARCANO ===")
+    print("1.- Stock por categoría")
+    print("2.- Búsqueda por precio")
+    print("3.- Actualizar precio")
+    print("4.- Salir")
+
+    opcion = int(input("Ingrese la opción a elegir"))
+
+    if opcion == 1:
