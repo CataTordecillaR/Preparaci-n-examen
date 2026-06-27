46ac9932adf7b2e29c4dc83e6eaee8c8c4108925
CataTordecillaR <cata.tordecilla@gmail.com>
2026-06-26 20:21:22 -0400

    KJB,SADSQWD

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,7 +2,19 @@ items = {
     "MAG001" : ["Agonía de Escarcha", "Arma", "Legendaria", "Hielo"],
     "MAG002" : ["Anillo de Sauron", "Accesorio", "Epica", "Magia"],
     "MAG003" : ["Poción curativa", "Consumible", "Comun", "Vida"],
-    
+    "MAG004" : ["Corona Nemesis", "Armadura", "Legendaria", "Dragon"],
+    "MAG005" : ["Baston Lunar de Lileath", "Arma", "Epica", "Hechizo"],
+    "MAG006" : ["Cristal de Hielo", "Material", "Rara", "Hielo"]
+}
+
+inventario = {
+    "MAG001" : [5000,3],
+    "MAG002" : [2500,8],
+    "MAG003" : [100,50],
+    "MAG004" : [10000,1],
+    "MAG005" : [3500,4],
+    "MAG006" : [800,0]
+
 }
 
 while True:
